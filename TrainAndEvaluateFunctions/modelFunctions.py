c71574c1d41da419b50275d6a2de2cd96a00b2e5
Unknown <pgavigan@gmail.com>
2018-05-28 15:45:59 -0400

    Encapsulation of model generation
    Want to encapsulate model generation, training, and evaluation in order to use nested cross-validation.

---
+++ TrainAndEvaluateFunctions/modelFunctions.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 15:32:58 2018
+
+@author: patrickgavigan
+"""
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+
+
+# Create the model
+def defineModel(top_words, max_review_length):
+    embedding_vecor_length = 32
+    model = Sequential()
+    model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
+    model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(LSTM(100))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model;
--- TrainAndEvaluateFunctions/trainAndEvaluateMain.py
+++ TrainAndEvaluateFunctions/trainAndEvaluateMain.py
@@ -20,8 +20,12 @@ from keras.layers.convolutional import MaxPooling1D
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 
+from modelFunctions import defineModel
+
 # fix random seed for reproducibility
 numpy.random.seed(7)
+
+# Load the data and preprocess
 
 # load the dataset but only keep the top n words, zero the rest
 top_words = 5000
@@ -32,16 +36,19 @@ max_review_length = 500
 X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
 X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
 
-# create the model
-embedding_vecor_length = 32
-model = Sequential()
-model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
-model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
-model.add(MaxPooling1D(pool_size=2))
-model.add(LSTM(100))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
+# Create the model
+model = defineModel(top_words, max_review_length)
+#embedding_vecor_length = 32
+#model = Sequential()
+#model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
+#model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+#model.add(MaxPooling1D(pool_size=2))
+#model.add(LSTM(100))
+#model.add(Dense(1, activation='sigmoid'))
+#model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#print(model.summary())
+
+# Train the model
 model.fit(X_train, y_train, epochs=3, batch_size=64)
 
 # Final evaluation of the model
