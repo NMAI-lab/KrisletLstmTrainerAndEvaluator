35f8c5a03321e5b22e6f96dbd2f2f4c0b603fda4
Patrick Gavigan <pgavigan@gmail.com>
2018-06-19 14:15:37 -0400

    Updates to parser - made more functional

---
+++ TrainAndEvaluateFunctions/messWithData.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 19 13:54:43 2018
+
+@author: patrickgavigan
+"""
+
+from parsingFunctions import parseFile
+
+(x,y) = parseFile()
---
+++ TrainAndEvaluateFunctions/parsingFunctions.py
@@ -0,0 +1,15 @@
+#parses a single line of bayesian.txt e.g. 0.1, 0.2, 0.3
+def parse(line):
+    line = [float(i) for i in line.split(',')]
+    lastCharacter = len(line) - 1
+    return [line[:lastCharacter], [line[lastCharacter]]]
+
+def parseFile(fileName = "logFile.txt"):
+    x = list()
+    y = list()
+    with open(fileName) as f:
+        for line in f:
+            (currentX, currentY) = (parse(line))
+            x.append(currentX)
+            y.append(currentY[0])
+    return (x, y)
