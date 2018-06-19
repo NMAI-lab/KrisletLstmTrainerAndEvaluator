7baad667f93aa5052f1213b9b2547d2826634204
Patrick Gavigan <pgavigan@gmail.com>
2018-06-19 16:46:48 -0400

    Working on the LSTM history input data
    incomplete

--- TrainAndEvaluateFunctions/messWithData.py
+++ TrainAndEvaluateFunctions/messWithData.py
@@ -5,6 +5,7 @@ Created on Tue Jun 19 13:54:43 2018
 @author: patrickgavigan
 """
 
-from parsingFunctions import parseFile
+from parsingFunctions import parseFile, buildHistory
 
 (x,y) = parseFile()
+(newX, newY) = buildHistory(x, y)
--- TrainAndEvaluateFunctions/parsingFunctions.py
+++ TrainAndEvaluateFunctions/parsingFunctions.py
@@ -1,3 +1,6 @@
+import numpy as np
+import random
+
 #parses a single line of bayesian.txt e.g. 0.1, 0.2, 0.3
 def parse(line):
     line = [float(i) for i in line.split(',')]
@@ -13,3 +16,22 @@ def parseFile(fileName = "logFile.txt"):
             x.append(currentX)
             y.append(currentY[0])
     return (x, y)
+
+def buildHistory(x, y, depth = 5):
+    
+    numFeatures = len(x[0]) + 1
+    numSamples = len(y)
+    numNewSamples = numSamples - (depth - 1)
+    
+    newX = np.zeros((depth, numFeatures, numNewSamples))
+    newY = np.zeros(numNewSamples)
+    firstY = random.choice(y)
+    
+    for i in range (numNewSamples - 1):
+        for j in range (numFeatures - 1):
+            for k in range (depth - 1):
+                newX[k,j,i] = x[k,j+i]
+                newY[i] = y[i + depth]
+        
+    
+    return newX, newY
