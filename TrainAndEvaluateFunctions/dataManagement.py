98befa76fc656604e0f5225688d5b83e7ba1f624
Patrick Gavigan <pgavigan@gmail.com>
2018-06-21 16:02:33 -0400

    Started integration of parsing functions with main code
    not finished

--- TrainAndEvaluateFunctions/dataManagement.py
+++ TrainAndEvaluateFunctions/dataManagement.py
@@ -8,8 +8,8 @@ Created on Tue May 29 09:35:58 2018
 import numpy as np
 from keras.datasets import imdb
 from keras.preprocessing import sequence
-
 from sklearn.model_selection import StratifiedShuffleSplit
+from parsingFunctions import parseFile
 
 def getData():
 
@@ -38,6 +38,13 @@ def getData():
     # Return result
     return (X_train, y_train), (X_test, y_test), (x, y)
 
+def getLogFileData():
+    (x,y) = parseFile()
+    depth = 2
+    (newX, newY) = buildSequenceDataSet(x, y, depth)
+    return (newX,newY)
+    
+
 def stratefiedSplit(x, y):
     nSplits = 1
     testProportion = 0.2
@@ -49,3 +56,63 @@ def stratefiedSplit(x, y):
         yTrain, yTest = y[trainIndex], y[testIndex]
         
     return (xTrain, yTrain), (xTest, yTest)
+
+# Builds the dequence data set using the provided depth setting
+def buildSequenceDataSet(x, y, maxDepth = 3):
+    # Use lists (in order to append the next element)
+    xList = list()
+    yList = list()
+    
+    # Add the previous y to the current x features
+    x = addPreviousYAsFeature(x,y)
+    
+    # Iterate through the elements
+    for index in range(len(x)):
+        (currentX, currentY) = buildSingleSequence(x, y, index, maxDepth)
+        xList.append(currentX)
+        yList.append(currentY)
+        
+    # Convert to arrays
+    newX = np.asarray(xList)
+    newY = np.asarray(yList)
+
+    # Return results
+    return newX, newY
+
+# Build a history sequence based on given x, y for a given index of x. 
+# The depth specifies how far back in history to go
+def buildSingleSequence(x, y, index, depth):
+    # Set parameters (number of features)
+    numFeatures = len(x[0])
+    
+    # Setup return values
+    sequenceY = y[index]
+    sequenceX = np.zeros((numFeatures, depth))
+    
+    # Iteratively build the return values (iterates through the depth)
+    j = depth
+    for currentDepth in range(index, index-depth, -1):
+        j = j - 1
+        if currentDepth < 0:
+            # deal with case where there is no more data and we need to pad
+            currentX = np.zeros(numFeatures)
+        else:
+            # Get current x array of features
+            currentX = x[currentDepth]
+        
+        # Copy current x features to the output matrix
+        for i in range(0, numFeatures):
+            sequenceX[i,j] = currentX[i]            
+            
+    # Return the results
+    return (sequenceX, sequenceY)
+    
+# Adds the result for the previous steps's Y as a feature of the current step 
+# in X
+def addPreviousYAsFeature(x,y):
+    for i in range(len(x)):
+        if i == 0:
+            x[i].append(random.choice(y))
+        else:
+            x[i].append(y[i-1])
+    return x
