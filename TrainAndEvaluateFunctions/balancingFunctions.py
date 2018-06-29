ae86fa0f13251aea1eb9973e21ddbf4b61f5753f
Patrick Gavigan <pgavigan@gmail.com>
2018-06-29 17:07:43 -0400

    Implemented undersamplint
    Needs testing

---
+++ TrainAndEvaluateFunctions/balancingFunctions.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 27 15:19:11 2018
+
+@author: patrickgavigan
+"""
+
+from collections import Counter
+#from imblearn.over_sampling import SMOTE 
+import numpy as np
+import random
+
+# Perform random undersampling
+def underSample(data):
+    # Set parameters
+    (x,y) = data
+    balance = checkBalance(data)
+    numSamplesEachClass = min(balance)
+    numClasses = len(balance)
+    newDataLength = numClasses * numSamplesEachClass
+    xShape = np.shape(x)
+    
+    # Build arrays for output data
+    xNew = np.zeros((newDataLength, xShape[1], xShape[2]))
+    yNew = np.zeros(newDataLength)
+    
+    # Undersample each class
+    i = 0
+    for currentClass in range(numClasses):
+        # Get the indeces of the elements we are keeping for this class
+        indexList = getUnderSampledClassIndeces(y, currentClass, numSamplesEachClass)
+        
+        # Copy relevant values from original list
+        for j in range(len(indexList)):
+            xNew[i] = x[j]
+            yNew[i] = y[j]
+            i = i + 1
+    
+    # Shuffle the data
+    newData = shuffleOrder((xNew, yNew))
+    
+    # Return undersampled data
+    return (newData)
+        
+
+# Get the undersampled indeces for the specified class
+def getUnderSampledClassIndeces(y, classID, numToKeep):
+    yList = y.tolist()
+    classIndeces = yList.index(classID)
+    keepIndeces = random.sample(classIndeces, numToKeep)
+    return keepIndeces
+
+# Shuffles the order of the data elements
+def shuffleOrder(data):
+    (x,y) = data
+    xNew = np.zeros(np.shape(x))
+    yNew = np.zeros(np.shape(y))
+    maxRange = len(y)
+    newOrder = random.sample(range(0, maxRange), maxRange)
+    
+    for i in range(maxRange):
+        j = newOrder[i]
+        xNew[i] = x[j]
+        yNew[i] = y[j]
+    
+    return (xNew, yNew)
+    
+
+# Perform oversampling on the data using SMOTE
+#def overSample(data):
+#    balance = checkBalance(data)
+#    numSamplesEachClass = max(balance)
+#    
+#    return (X_res, y_res)
+
+# Measures the distance between two data points: a and b. Assumes that both
+# are members of the same class
+#def getDistance(a,b):
+    
+
+# Check the balance of the classes. Returns list of how many items data points 
+# for each class exist.
+def checkBalance(data):
+    y = data[1].astype(int, copy = True)
+    balanceCounter = Counter(y)
+    
+    balance = list()
+    for i in range(len(balanceCounter)):
+        balance.append(balanceCounter[i])
+        
+    return balance
--- TrainAndEvaluateFunctions/messWithData.py
+++ TrainAndEvaluateFunctions/messWithData.py
@@ -7,9 +7,15 @@ Created on Tue Jun 19 13:54:43 2018
 
 from parsingFunctions import loadData
 from dataManagement import convertToCategorical
+from balancingFunctions import oversample, checkBalance
 
 testType = "stateBasedKrislet"
 depth = 2
 data = loadData(testType, depth)
 
-yCategorical = convertToCategorical(data[1])
+balancedData = oversample(data)
+
+originalBalance = checkBalance(data)
+newBalance = checkBalance(balancedData)
+
+#yCategorical = convertToCategorical(data[1])
