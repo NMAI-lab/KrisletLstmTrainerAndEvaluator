5810f11358dfd5d9eba0ef23caf84407939e7c03
Patrick Gavigan <pgavigan@gmail.com>
2018-07-17 14:30:01 -0400

    Removed categorical conversion - It shouldn't have been put back in :)

--- TrainAndEvaluateFunctions/parsingFunctions.py
+++ TrainAndEvaluateFunctions/parsingFunctions.py
@@ -1,7 +1,7 @@
 from os import listdir
 import numpy as np
 
-from dataManagement import buildSequenceDataSet, convertToCategorical
+from dataManagement import buildSequenceDataSet
 
 def loadData(testType, depth):
     fileNames = getFileNames(testType)
@@ -16,7 +16,6 @@ def loadData(testType, depth):
             x = np.append(x, xCurrent, axis = 0)
             y = np.append(y, yCurrent, axis = 0)
         
-    y = convertToCategorical(y)
     return (x,y)
     
 
