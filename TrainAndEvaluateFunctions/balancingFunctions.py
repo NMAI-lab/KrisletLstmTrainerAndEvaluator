0c889b3f086a3402b087c8799103f6dda9c1d8dc
Patrick Gavigan <pgavigan@gmail.com>
2018-07-31 10:12:28 -0400

    Corrected issue with circular dependencies

--- TrainAndEvaluateFunctions/balancingFunctions.py
+++ TrainAndEvaluateFunctions/balancingFunctions.py
@@ -9,7 +9,6 @@ from constants import getRandomSeed
 from collections import Counter
 #from imblearn.over_sampling import SMOTE 
 
-from underSampling import randomUnderSample
 
 import numpy as np
 import random
@@ -61,3 +60,58 @@ def checkBalance(data):
         balance.append(balanceCounter[i])
         
     return balance
+
+# Perform random undersampling
+def randomUnderSample(data):
+    # Set parameters
+    (x,y) = data
+    balance = checkBalance(data)
+    numSamplesEachClass = min(balance)
+    numClasses = len(balance)
+    newDataLength = numClasses * numSamplesEachClass
+    xShape = np.shape(x)
+    
+    # Build arrays for output data
+    xNew = np.zeros((newDataLength, xShape[1], xShape[2]))
+    yNew = np.zeros(newDataLength)
+    
+    # Undersample each class
+    i = 0
+    for currentClass in range(numClasses):
+        # Get the indeces of the elements we are keeping for this class
+        indexList = getUnderSampledClassIndeces(y, currentClass, numSamplesEachClass)
+        
+        # Copy relevant values from original list
+        for j in range(len(indexList)):
+            currentIndex = indexList[j]
+            xNew[i] = x[currentIndex]
+            yNew[i] = y[currentIndex]
+            i = i + 1
+    
+    # Shuffle the data
+    newData = shuffleOrder((xNew, yNew))
+    
+    # Return undersampled data
+    return (newData)
+
+
+# Get the undersampled indeces for the specified class.
+def getUnderSampledClassIndeces(y, classID, numToKeep):
+    classIndeces = getIndecesOfValueInData(y, classID)
+    if (len(classIndeces) <= numToKeep):
+        keepIndeces = classIndeces
+    else:
+        random.seed(getRandomSeed())
+        keepIndeces = random.sample(classIndeces, numToKeep)
+    return keepIndeces
+
+# Perform oversampling on the data using SMOTE
+#def overSample(data):
+#    balance = checkBalance(data)
+#    numSamplesEachClass = max(balance)
+#    
+#    return (X_res, y_res)
+
+# Measures the distance between two data points: a and b. Assumes that both
+# are members of the same class
+#def getDistance(a,b):
