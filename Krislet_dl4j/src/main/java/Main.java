8304f49712db08850ee23705e4a43a375bd40409
Unknown <pgavigan@gmail.com>
2019-01-19 20:46:20 -0500

    Update to POM file. Still not working

--- Krislet_dl4j/src/main/java/Main.java
+++ Krislet_dl4j/src/main/java/Main.java
@@ -3,8 +3,8 @@
 //import org.deeplearning4j.nn.graph.ComputationGraph;
 //import org.deeplearning4j.nn.modelimport.keras.KerasLayer;
 import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
-//import org.nd4j.linalg.api.ndarray.INDArray;
-//import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 
 //import java.io.File;
@@ -26,36 +26,7 @@ public class Main {
 
 
     public static void main(String[] args) throws Exception {
-
-
-        // First, register the Keras layer wrapped around our custom SameDiff attention layer
-        //KerasLayer.registerCustomLayer("AttentionWeightedAverage", KerasDeepMojiAttention.class);
-
-        // Then, download the model from azure (check if it's cached)
-        //File directory = new File(DATA_PATH);
-        //if(!directory.exists()) directory.mkdir();
-
-        //String modelUrl = "http://blob.deeplearning4j.org/models/deepmoji.h5";
-        //String filePath = DATA_PATH + "model.h5";
-        //File cachedKerasFile = new File(filePath);
-
-        //if (!cachedKerasFile.exists()) {
-        //    System.out.println("Downloading model to " + cachedKerasFile.toString());
-        //    FileUtils.copyURLToFile(new URL(modelUrl), cachedKerasFile);
-        //    System.out.println("Download complete");
-        //    cachedKerasFile.deleteOnExit();
-        //}
-
-        // Finally, import the model and test on artificial input data.
-        //ComputationGraph graph = KerasModelImport.importKerasModelAndWeights(cachedKerasFile.getAbsolutePath());
-        //ComputationGraph graph = KerasModelImport.importKerasModelAndWeights(new ClassPathResource("C:\\Users\\Patrick\\Desktop\\model.h5"));
-        //INDArray input = Nd4j.create(new int[] {10, 30});
-        //graph.output(input);
-
-        //String simpleMlp = new ClassPathResource("model.h5").getFile().getPath();
-        //MultiLayerNetwork model = KerasModelImport.importKerasSequentialModelAndWeights(simpleMlp);
-        MultiLayerNetwork model = KerasModelImport.importKerasSequentialModelAndWeights("C:\\Users\\patrickgavigan\\Documents\\GitHub\\KrisletLstmTrainerAndEvaluator\\Krislet_dl4j\\models\\model.h5");
-        //System.out.println(model.getInput());
+        MultiLayerNetwork model = KerasModelImport.importKerasSequentialModelAndWeights("C:\\Users\\Patrick\\Documents\\GitHub\\KrisletLstmTrainerAndEvaluator\\Krislet_dl4j\\models\\model.h5");
         System.out.println(model.summary());
         System.out.println("Example completed.");
     }
